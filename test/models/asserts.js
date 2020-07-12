a3950dedaa45788eeec5e9d825f7c7533a64243e
uldiszzTDL <59832690+uldiszzTDL@users.noreply.github.com>
2020-07-12 18:01:09 +0300

    Change the naming of min and max video bitrate assertions
    * Update video mean bitrate assert wording

--- test/models/asserts.js
+++ test/models/asserts.js
@@ -284,7 +284,7 @@ module.exports = class Asserts {
       'gte'
     );
     this.assert(
-      'Video max mean bitrate',
+      'Video mean bitrate',
       streamStats.bitrateMean,
       config.videoAssertProfile.maxBitrateMeanKps,
       'lte'
